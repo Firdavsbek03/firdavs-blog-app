41964d6b3c38e72f6e646d9f76d0931fc1ab6f4b
Jalolov Firdavs <jalolov.firdavs0809@gmail.com>
2022-08-18 11:11:46 +0500

    noting to say

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class User(UserMixin,db.Model):
     __tablename__ = "users"
     id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(250),nullable=False,unique=True)
-    password=db.Column(db.String(50),nullable=False)
+    password=db.Column(db.String(350),nullable=False)
     name=db.Column(db.String(250),nullable=False)
     posts=relationship("BlogPost",back_populates='author')
     # Relational db with comment and user
